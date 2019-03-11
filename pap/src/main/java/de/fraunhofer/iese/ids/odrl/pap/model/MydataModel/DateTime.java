fe168b70db0c00b3ae445beff51e67abfee77a2b
hosseinz <arghavan.hosseinzadeh@iese.fraunhofer.de>
2019-03-11 15:14:53 +0100

    dateTime updated in MYDATA policy

--- pap/src/main/java/de/fraunhofer/iese/ids/odrl/pap/model/MydataModel/DateTime.java
+++ pap/src/main/java/de/fraunhofer/iese/ids/odrl/pap/model/MydataModel/DateTime.java
@@ -26,7 +26,28 @@ public class DateTime {
 
     @Override
     public String toString() {
-        return  "          <dateTime is='"+ is.getMydataInterval() +"' value='"+ getMydataDateTime() +"'/> \r\n";
+        if(this.is.equals(IntervalCondition.GT))
+        {
+            return "          <or> \r\n" +
+                    "            <and> \r\n" +
+                    "              <date is='exactly' value='"+ getDate(getMydataDateTime()) +"'/> \r\n" +
+                    "              <time is='"+ is.getMydataInterval() +"' value='"+ getTime(getMydataDateTime()) +"'/> \r\n" +
+                    "            </and> \r\n" +
+                    "            <date is='"+ is.getMydataInterval() +"' value='"+ getDate(getMydataDateTime()) +"'/> \r\n" +
+                    "          </or> \r\n";
+        }else if(this.is.equals(IntervalCondition.LT))
+        {
+            return "          <or> \r\n" +
+                    "            <date is='"+ is.getMydataInterval() +"' value='"+ getDate(getMydataDateTime()) +"'/> \r\n" +
+                    "            <and> \r\n" +
+                    "              <date is='exactly' value='"+ getDate(getMydataDateTime()) +"'/> \r\n" +
+                    "              <time is='"+ is.getMydataInterval() +"' value='"+ getTime(getMydataDateTime()) +"'/> \r\n" +
+                    "            </and> \r\n" +
+                    "          </or> \r\n";
+        }
+        return "";
+        // We may implement dateTime function for MYDATA in near future
+        //return  "          <dateTime is='"+ is.getMydataInterval() +"' value='"+ getMydataDateTime() +"'/> \r\n";
     }
 
 
@@ -37,4 +58,15 @@ public class DateTime {
         return timestamp.toString();
     }
 
+    private String getDate(String dateTime)
+    {
+        String[] parts = dateTime.split(" ");
+        return parts[0];
+    }
+
+    private String getTime(String dateTime)
+    {
+        String[] parts = dateTime.split(" ");
+        return parts[1];
+    }
 }
