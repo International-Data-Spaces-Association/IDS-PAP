b87967f406d9babccf440394e94bad0d713b6c07
Tom Kollmer <>
2022-01-11 09:56:50 +0100

    - API tests are now working

--- src/main/ui/src/components/backend/Submit.js
+++ src/main/ui/src/components/backend/Submit.js
@@ -11,17 +11,18 @@ export default function Submit(url, values, states, setErrors, history ,e) {
     const isoValues = convertDateToIso(values, states)
     axios.post(BASE_URL + url, isoValues)
     .then((response) => {
-      let policies = response.data.split('DTPOLICY:');
-      var dict = {
-        jsonPolicy: "",
-        dtPolicy: "",
-      }
-      dict.jsonPolicy = policies[0];
-      dict.dtPolicy = policies[1];
+      axios.post(BASE_URL + "/policy/initialTDP", response.data)
+      .then((responseTDP) => {
+              var dict = {
+                jsonPolicy: JSON.stringify(response.data, null, 2),
+                dtPolicy: responseTDP.data,
+              }
       history.push({
         pathname: '/ODRLCreator',
         state: dict
-      })
+      })}, (error) => {
+        console.log(error);
+      })    
     }, (error) => {
       console.log(error);
     })    
