ae36bf19deeab7b20803e1928c750e8706001619
Tom Kollmer <>
2022-04-08 17:09:36 +0200

    TODO

--- src/main/java/de/fraunhofer/iese/ids/odrl/pap/repository/IPolicyRepo.java
+++ src/main/java/de/fraunhofer/iese/ids/odrl/pap/repository/IPolicyRepo.java
@@ -12,6 +12,7 @@ import de.fraunhofer.iese.ids.odrl.pap.entity.Policy;
 
 @Repository
 public interface IPolicyRepo extends JpaRepository<Policy, Long> {
+	// TODO:  
 	//@Query(value = "SELECT ID, POLICYID, DESCRIPTION, POLICY_TYPE  FROM TBL_POLICY", nativeQuery = true)
 	//List<Object[]> findAllShort();
 }
